92842411e222ecb62a824450e108d0d5d6dd4d9d
tonegas <tonegas@gmail.com>
2021-11-16 01:12:18 +0100

    recurrent network plot conpleted

--- complex_oscillator.py
+++ complex_oscillator.py
@@ -5,18 +5,18 @@ massamollasmorzatore = Neu4mes()
 x = Input('x')
 y = Input('y')
 F = ControlInput('force')
-F2 = Input('force2')
-relation = lambda: Linear(y.tw(2))+Linear(x.tw(1.2))+Linear(F)+Linear(F2)
+# F2 = Input('force2')
+relation = lambda: Linear(x.tw(2))+Linear(y.tw(2))+Linear(F)
 
 x_z = Output(x.z(-1), relation())
-y_z = Output(y.z(-1), relation())
+# y_z = Output(y.z(-1), relation())
 massamollasmorzatore.addModel(x_z)
-massamollasmorzatore.addModel(y_z)
+# massamollasmorzatore.addModel(y_z)
 
-massamollasmorzatore.neuralizeModel(0.05, prediction_window = 60)
+massamollasmorzatore.neuralizeModel(0.05, prediction_window = 50)
 
 data_struct = ['time','x','y','force','force2']
 data_folder = './structured_nn-code/data/pendulum-b/data/'
 massamollasmorzatore.loadData(data_struct, folder = data_folder)
 
-massamollasmorzatore.trainModel(states = [x_z,y_z], validation_percentage = 10, show_results = True)
+massamollasmorzatore.trainModel(states = [x_z], validation_percentage = 10, show_results = True)
