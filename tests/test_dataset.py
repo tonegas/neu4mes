c9277679670638a577221c221a465a4696031189
tonegas <tonegas@gmail.com>
2022-01-14 19:52:56 +0100

    debug path travis

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -12,6 +12,11 @@ class Neu4mesCreateDatasetTest(unittest.TestCase):
         relation = Linear(input.tw(0.05))
         fun = Output(output.z(-1),relation)
 
+        print('----------------------------------------------------------------')
+        print(data_folder)
+        print('----------------------------------------------------------------')
+
+
         test = Neu4mes()
         test.addModel(fun)
         test.neuralizeModel(0.01)
