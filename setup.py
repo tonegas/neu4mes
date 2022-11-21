9cf49385e15441f23b557d6412c70248934be9f0
Sebastiano Taddei <sebastianotaddei@gmail.com>
2022-11-21 09:02:33 +0100

    Branched project
    Created neu4mes_2,0 branch to continue the development of the neu4mes library.

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
-import setuptools
+'''
+Authors : Gastone Pietro Rosati Papini, Sebastiano Taddei
+Date    : 31/10/2022
+License : MIT
+'''
+
+from setuptools import find_packages, setup
 
 from neu4mes import __version__
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md', 'r', encoding='utf-8') as fh:
+    readme_file = fh.read()
 
-setuptools.setup(
-    name="neu4mes",
+with open('LICENSE', 'r', encoding='utf-8') as fh:
+    license_file = fh.read()
+
+setup(
+    name='neu4mes',
     version=__version__,
-    author="Gastone Pietro Rosati Papini",
-    author_email="tonegas@gmail.com",
-    description="Mechanics-informed neural network framework for modeling and control mechanical system",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tonegas/neu4mes",
-    packages=setuptools.find_packages(),
+    author='Gastone Pietro Rosati Papini, Sebastiano Taddei',
+    author_email='tonegas@gmail.com, sebastianotaddei@gmail.com',
+    description=('Mechanics-informed neural network framework ',
+                 'for modeling and control mechanical system'),
+    long_description=readme_file,
+    long_description_content_type='text/markdown',
+    url='https://github.com/tonegas/neu4mes',
+    license=license_file,
+    packages=find_packages(exclude=('docs', 'examples', 'tests')),
     platforms='any',
-    packages=["neu4mes"],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
-    install_requires=[ 'keras', 'tensorflow','numpy','pyyaml'],
-    python_requires='>3.6'
+    install_requires=['numpy','pandas','tensorflow','pyyaml'],
+    python_requires='>3.10.6'
 )
