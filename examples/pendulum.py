e1db50dfcb9109c9f6a5c2c5211f4de33193a376
tonegas <tonegas@gmail.com>
2024-03-25 15:14:55 +0100

    Add pendolum model

--- examples/pendulum.py
+++ examples/pendulum.py
@@ -1,24 +1,25 @@
 from neu4mes import *
+from neu4mes.visualizer import StandardVisualizer
 
-pendolo = Neu4mes()
+# Create neu4mes structure
+pendolum = Neu4mes(verbose = True, visualizer = StandardVisualizer())
 
+# Create neural model
 theta = Input('theta')
 T     = Input('torque')
-
 lin_theta = Linear(theta.tw(1.5))
 sin_theta = LinearBias(Sin(theta))
 torque = Linear(T)
-
 theta_z = Output(theta.z(-1), lin_theta+sin_theta+torque)
 
-pendolo.addModel(theta_z)
+# Add the neural model to the neu4mes structure and neuralization of the model
+pendolum.addModel(theta_z)
+pendolum.neuralizeModel(0.05)
 
-pendolo.neuralizeModel(0.05)
-
-
+# Data load
 data_struct = ['time','theta','theta_s','','','torque']
-data_folder = './data/data-pendulum-b/'
+data_folder = './datasets/pendulum/data/'
+pendolum.loadData(data_struct, folder = data_folder)
 
-pendolo.loadData(data_struct, folder = data_folder)
-
-pendolo.trainModel(validation_percentage = 30, show_results = True)
+# Neural network train
+pendolum.trainModel(test_percentage = 30, show_results = True)
