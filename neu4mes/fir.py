38f0e00affdf99ddefbda690b2604d4248d033be
tonegas <tonegas@gmail.com>
2024-06-13 18:35:42 +0200

    fixed the dimension of the parameter of fir

--- neu4mes/fir.py
+++ neu4mes/fir.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch
 
 from neu4mes.relation import NeuObj, Stream, AutoToStream, merge
+from neu4mes.utilis import check
 from neu4mes.input import Input
 from neu4mes.model import Model
 from neu4mes.parameter import Parameter
@@ -11,34 +12,44 @@ from neu4mes.parameter import Parameter
 fir_relation_name = 'Fir'
 
 class Fir(NeuObj, AutoToStream):
-    def __init__(self, output_dimension = 1, parameter = None):
+    def __init__(self, output_dimension = None, parameter = None):
         self.relation_name = fir_relation_name
-        self.output_dimension = output_dimension
         self.parameter = parameter
 
         if parameter is None:
+            self.output_dimension = 1 if output_dimension is None else output_dimension
             super().__init__('P' + fir_relation_name + str(NeuObj.count))
             self.json['Parameters'][self.name] = { 'dim': self.output_dimension }
         else:
-            assert type(parameter) is Parameter, 'input parameter must be of type Parameter'
-            assert parameter.dim['dim'] == self.output_dimension, 'output_dimension must be equal to dim of the parameter'
+            check(type(parameter) is Parameter, TypeError, 'Input parameter must be of type Parameter')
+            if output_dimension is None:
+                check(type(parameter.dim['dim']) is int, TypeError, 'Dimension of the parameter must be an integer for the Fir')
+                self.output_dimension = parameter.dim['dim']
+            else:
+                self.output_dimension = output_dimension
+                check(parameter.dim['dim'] == self.output_dimension, ValueError, 'output_dimension must be equal to dim of the Parameter')
             super().__init__(parameter.name)
             self.json['Parameters'][self.name] = copy.deepcopy(parameter.dim)
 
     def __call__(self, obj):
         stream_name = fir_relation_name + str(Stream.count)
         #TODO remove this limit the input can have different dimension
-        #The output dimensions will be equal to the input dimension
-        assert 'dim' in obj.dim and obj.dim['dim'] == 1, 'Input dimension must be scalar'
+        check('dim' in obj.dim and obj.dim['dim'] == 1, ValueError, 'Input dimension must be scalar')
         window = 'tw' if 'tw' in obj.dim else ('sw' if 'sw' in obj.dim else None)
         if window:
             if self.parameter:
-                assert self.json['Parameters'][self.name][window] == obj.dim[window], 'Time window of the input dimension must be the same of the parameter'
+                check(window in self.json['Parameters'][self.name],
+                      KeyError,
+                      f"The window \'{window}\' of the input is not in the parameter")
+                check(self.json['Parameters'][self.name][window] == obj.dim[window],
+                      ValueError,
+                      f"The window \'{window}\' of the input must be the same of the parameter")
             else:
                 self.json['Parameters'][self.name][window] = obj.dim[window]
         else:
             if self.parameter:
-                assert window not in self.json['Parameters'][self.name], 'The parameter have a time window and the input no'
+                cond = 'sw' not in self.json['Parameters'][self.name] and 'tw' not in self.json['Parameters'][self.name]
+                check(cond, KeyError,'The parameter have a time window and the input no')
 
         stream_json = merge(self.json,obj.json)
         if type(obj) is Input or type(obj) is Stream:
