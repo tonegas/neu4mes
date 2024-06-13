aad2a792ac3a6a67276f13db9796aa3f09b3c446
tonegas <tonegas@gmail.com>
2024-06-13 18:36:17 +0200

    Added the sw parameter

--- neu4mes/parameter.py
+++ neu4mes/parameter.py
@@ -5,11 +5,13 @@ from neu4mes.relation import NeuObj, Stream
 #values = zeros, ones, 1order, linear, quadratic
 
 class Parameter(NeuObj, Stream):
-    def __init__(self, name:str, dimensions = 1, tw = None):
+    def __init__(self, name:str, dimensions = 1, tw = None, sw = None):
         NeuObj.__init__(self, name)
         self.dim = {'dim': dimensions}
         if tw is not None:
             self.dim['tw'] = tw
+        if sw is not None:
+            self.dim['sw'] = sw
         # deepcopy dimention information inside Parameters
         self.json['Parameters'][self.name] = copy.deepcopy(self.dim)
 
