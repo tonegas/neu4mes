f3fcee873388a5908bb2f16e4244704d784d42a1
tonegas <tonegas@gmail.com>
2024-06-19 01:17:29 +0200

    Added tanh

--- neu4mes/activation.py
+++ neu4mes/activation.py
@@ -5,6 +5,7 @@ from neu4mes.relation import Stream, ToStream
 from neu4mes.model import Model
 
 relu_relation_name = 'ReLU'
+tanh_relation_name = 'Tanh'
 
 class Relu(Stream, ToStream):
     def __init__(self, obj):
@@ -14,8 +15,19 @@ class Relu(Stream, ToStream):
         else:
             raise Exception('Type is not supported!')
 
+class Tanh(Stream, ToStream):
+    def __init__(self, obj):
+        super().__init__(tanh_relation_name + str(Stream.count),obj.json,obj.dim)
+        if type(obj) is Stream:
+            self.json['Relations'][self.name] = [tanh_relation_name,[obj.name]]
+        else:
+            raise Exception('Type is not supported!')
+
+def createTanh(self, *input):
+    return nn.Tanh()
 
 def createRelu(self, *input):
     return nn.ReLU()
 
 setattr(Model, relu_relation_name, createRelu)
+setattr(Model, tanh_relation_name, createTanh)
