dfdcc1fce1853c21691fbcfa9d37e93e4a7507b1
tonegas <tonegas@gmail.com>
2024-11-13 22:12:15 +0100

    started the documentation on the new project nnodely

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'nnodely'
+author = 'tonegas'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
