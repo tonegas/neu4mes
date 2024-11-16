0ea89146eb64d1be3a5b83b3cab58d771d1184f8
tonegas <tonegas@gmail.com>
2024-11-16 18:09:16 +0100

    new test

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,7 @@ sys.path.insert(0, os.path.join(dirname, ".."))
 project = 'nnodely'
 author = 'tonegas'
 release = '1.0'
+version = '1.0'
 
 # -- General configuration ---------------------------------------------------
 
