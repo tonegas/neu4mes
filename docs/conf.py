c0918ca571bf5f3a4a996e949d258f0ad76b05d3
tonegas <tonegas@gmail.com>
2024-11-16 16:26:25 +0100

    Fixed the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 # -- Path setup --------------------------------------------------------------
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../neu4mes'))
+sys.path.insert(0, os.path.abspath('neu4mes'))
 
 # -- Project information -----------------------------------------------------
 
