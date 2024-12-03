20cd6ccf6559c3671eb26cd6cc2004f4863831e9
Giovanni Scialla <giovanni_scialla1999@outlook.it>
2024-12-03 17:00:47 +0100

    added max and mix functions for dictionaries

--- neu4mes/utils.py
+++ neu4mes/utils.py
@@ -97,5 +97,11 @@ def check(condition, exception, string):
 def argmax_max(iterable):
     return max(enumerate(iterable), key=lambda x: x[1])
 
+def argmax_dict(iterable: dict):
+    return max(iterable.items(), key=lambda x: x[1])
+
 def argmin_min(iterable):
     return min(enumerate(iterable), key=lambda x: x[1])
+
+def argmin_dict(iterable: dict):
+    return min(iterable.items(), key=lambda x: x[1])
